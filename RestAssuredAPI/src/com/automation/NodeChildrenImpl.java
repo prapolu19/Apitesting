f8fb637c4c11f1d356332201b3e16c5f4a5c176b
prapolu <prapolu@HYD-LAP-00954.corp.nisum.com>
2021-10-13 21:51:34 +0530

    first commit

---
+++ RestAssuredAPI/src/com/automation/NodeChildrenImpl.java
@@ -0,0 +1,10 @@
+package com.automation;
+
+public class NodeChildrenImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ RestAssuredAPI/src/com/automation/basic.java
@@ -0,0 +1,11 @@
+package com.automation;
+
+public class basic {
+
+	public static void main(String[] args) {
+		WebDriver Di
+
+
+	}
+
+}
